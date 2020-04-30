f81e2cafdf533f4df45a807efeaebf732c5d55d4
Alchemi <misha.smit@xs4all.nl>
2020-04-30 12:15:53 +0200

    Update ModMaterials.java

--- src/main/java/me/alchemi/vattghern/objects/ModMaterials.java
+++ src/main/java/me/alchemi/vattghern/objects/ModMaterials.java
@@ -3,7 +3,7 @@ package me.alchemi.vattghern.objects;
 import net.minecraft.item.Item.ToolMaterial;
 import net.minecraftforge.common.util.EnumHelper;
 
-public class ToolMaterialHolder {
+public class ModMaterials {
 
 	public static ToolMaterial DIMERITHIUM = EnumHelper.addToolMaterial("Dimerithium", 4, 1000, 10, 1, 0);
 	
